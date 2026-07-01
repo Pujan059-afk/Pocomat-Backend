8c56a11abde8ec4fd0e69a600f895bdb7754b6d9
PUJAN059 <githubpujan059@gmail.com>
2026-07-01 13:26:42 +0545

    Add age field to TeamMember model + update seed data

--- models/WebDevineersTeamMember.js
+++ models/WebDevineersTeamMember.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const wdTeamMemberSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  age: { type: Number, default: null },
   role: { type: String, required: true },
   specialization: { type: String, default: '' },
   image: { type: String, default: '' },
